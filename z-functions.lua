a774bb3c6cfea099fe18d53e6cdcf09629f9ab71
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2025-10-13 11:29:43 -0400

    Update z-functions.lua
    Add set_env_tint

--- z-functions.lua
+++ z-functions.lua
@@ -37,6 +37,9 @@ function lerp(a, b, t)
 end
 
 function replace_skybox(sky, p)
+  if sky[p.currAreaIndex] == nil then
+    return end
+  
 texture_override_set("Custom_Skybox_Icosphere_cloud_floor_rgba16", sky[p.currAreaIndex].front or prevNone)
         if sky[p.currAreaIndex].skytype == "box" then
         texture_override_set("Custom_Skybox_Cube_skyast_up_rgba16", sky[p.currAreaIndex].up or prevNone)
@@ -47,4 +50,25 @@ texture_override_set("Custom_Skybox_Icosphere_cloud_floor_rgba16", sky[p.currAre
        texture_override_set("Custom_Skybox_Cube_skyast_ft_rgba16", sky[p.currAreaIndex].front or prevNone)
         end
         
-        end
+end
+  
+  
+  --from environment tint by agent x 
+function set_env_tint(color, lightingDir)
+  if color == {nil} then
+    return end
+  if lightingDir == {nil} then
+    return end
+    set_lighting_color(0, color.r)
+    set_lighting_color(1, color.g)
+    set_lighting_color(2, color.b)
+    set_vertex_color(0, color.r)
+    set_vertex_color(1, color.g)
+    set_vertex_color(2, color.b)
+    set_fog_color(0, color.r)
+    set_fog_color(1, color.g)
+    set_fog_color(2, color.b)
+    set_lighting_dir(0, lightingDir.x)
+    set_lighting_dir(1, lightingDir.y)
+    set_lighting_dir(2, lightingDir.z)
+end
