27507f39ba1489ab90959616bf18bb1ab654bad4
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2026-03-10 21:29:31 -0400

    now it can spin

--- p-skybox.lua
+++ p-skybox.lua
@@ -7,14 +7,25 @@ local l = gLakituState
 
 function bhv_mapiskybox_init(o)
     o.oFlags = OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE
+    o.oFaceAngleYaw = 0x0
     o.header.gfx.skipInViewCheck = true
-    obj_scale(o, 9.9)
+    obj_scale(o, 101)
+    set_override_far(200000)
 end
 
 function bhv_mapiskybox_loop(o)
     o.oPosX = l.pos.x
     o.oPosY = l.pos.y
     o.oPosZ = l.pos.z
+    
+    if MAPi.get_cur_hangout() then
+    local turnRate = mapTable[curLevel].skybox[gNetworkPlayers[0].currAreaIndex].spin
+    
+    if turnRate then
+      o.oFaceAngleYaw = o.oFaceAngleYaw + turnRate
+    end
+    end
+    
 end
 
 id_bhvmapiskybox = hook_behavior(bhvmapiskybox, OBJ_LIST_LEVEL, false, bhv_mapiskybox_init, bhv_mapiskybox_loop)
