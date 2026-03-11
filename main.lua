412d36cd5cc5c4be22109fdd5012cf1d644f1269
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2026-03-10 21:35:06 -0400

    update

--- main.lua
+++ main.lua
@@ -2,14 +2,21 @@
 -- description: An API that let's you add and warp to custom levels
 -- pausable: false
 
-curBGM = nil
-curVolume = 1
-VOLUME_PAUSE = 0.35
+bgm = {
+  src = nil,
+  goalVolume = 1,
+  volume = 1
+}
+
+VOLUME_PAUSE = 0.25
 VOLUME_DEFAULT = 1
-VOLUME_MUTE = 0
+VOLUME_MUTE = 0.001
 curLevel = 1
 gGlobalSyncTable.canWarp = true
 gGlobalSyncTable.warpPopups = true
+gLevelValues.fixCollisionBugs = true
+gLevelValues.fixCollisionBugsRoundedCorners = false
+IS_ROMHACK = false
 
 local prevCG = get_texture_info("prev_cg")
 local prevCMF = get_texture_info("prev_cmf")
@@ -19,14 +26,9 @@ _G.MAPi_Active = true
 mapTable = {
 
   [1] = {
-    source = LEVEL_CASTLE_GROUNDS,
+    source = gLevelValues.entryLevel,
     name = "Castle Grounds",
-    description = {
-      "The starting area of the game!",
-      "walk around or go directly",
-      "into the castle to kick off this",
-      "adventure!"
-    },
+    description = "The starting area of the game! Walk around or go directly into the castle to kick off this great adventure!",
     credit = "Nintendo",
     prev = prevCG,
     entrysnd = SOUND_MENU_MARIO_CASTLE_WARP2,
@@ -43,10 +45,7 @@ mapTable = {
   [2] = {
     source = LEVEL_CASTLE,
     name = "Castle Main Floor",
-    description = {
-      "Princess Peach's castle.",
-      "The main area of the game,",
-    },
+    description = "Princess Peach's castle. The main area of the game",
     credit = "Nintendo",
     prev = prevCMF,
     entrysnd = SOUND_MENU_MARIO_CASTLE_WARP,
@@ -62,6 +61,7 @@ mapTable = {
 }
 
 nuhuhdontusethatone = {
+  [0] = true,
   [1] = true,
   [2] = true,
   [3] = true,
@@ -79,22 +79,26 @@ function warp_to_hangout(mapID, actID, warpNode)
       djui_popup_create("This map does not Exist!", 1)
       return true
     end
-    if mapTable[tonumber(mapID)].source == nil or nuhuhdontusethatone[mapTable[tonumber(mapID)].source] or (level_is_vanilla_level(mapTable[tonumber(mapID)].source) == false and smlua_level_util_get_info(mapTable[tonumber(mapID)].source) == nil) then
+    if mapTable[tonumber(mapID)].source == nil or nuhuhdontusethatone[mapTable[tonumber(mapID)].source] or (mapTable[tonumber(mapID)].source >= 50 and smlua_level_util_get_info(mapTable[tonumber(mapID)].source) == nil) then
       djui_popup_create("This map has an invalid levelNum!", 2)
         else
   
-         if curBGM ~= nil then
-      audio_stream_stop(curBGM)
-      curBGM = nil
+         if bgm.src ~= nil then
+      audio_stream_stop(bgm.src)
+      bgm.src = nil
     end 
-          warp_to_warpnode(mapTable[tonumber(mapID)].source, 1, actID or 1, warpNode or 0x0A)
-          Menu = false
-
+    local warped = warp_to_warpnode(mapTable[tonumber(mapID)].source, 1, actID or 1, warpNode or 0x0A)
+    if warped == true then
+        close_mapi_menu()
         curLevel = tonumber(mapID)
         if gGlobalSyncTable.warpPopups == true then
-          local plrCol = network_get_player_text_color_string(0)
-        djui_popup_create_global(plrCol..gNetworkPlayers[0].name.."\\#dcdcdc\\ entered \\#ffffff\\"..mapTable[mapID].name.."\\#dcdcdc\\, Act \\#ffffff\\#"..tostring(actID), 2)
+          local playerName =  network_get_player_text_color_string(0)..gNetworkPlayers[0].name
+          local mapName = "\\#"..string.format("%02X", mapTable[mapID].textColor.r)..string.format("%02X", mapTable[mapID].textColor.g)..string.format("%02X", mapTable[mapID].textColor.b).."\\"..mapTable[mapID].name
+          djui_popup_create_global(playerName.."\\#dcdcdc\\ entered "..mapName.."\\#dcdcdc\\, Act \\#ffffff\\#"..tostring(actID), 2)
         end
+    else
+      djui_popup_create("Failed to warp!", 2)
+    end
         end
         return true
     end
@@ -102,12 +106,11 @@ function warp_to_hangout(mapID, actID, warpNode)
 end
 
 local function custom_hangout_bgm(m, seq)
-
-if gMarioStates[0].playerIndex == 0 then
-  if curBGM ~= nil and seq == get_current_background_music() then
-return 0
-end
-end
+  if gMarioStates[0].playerIndex == 0 then
+    if bgm.src ~= nil and seq == get_current_background_music() then
+      return 0
+    end
+  end
 end
 
 hook_event(HOOK_ON_SEQ_LOAD, custom_hangout_bgm)
@@ -134,11 +137,32 @@ end
 
 hook_event(HOOK_ON_LEVEL_INIT, play_init_sound)
 
-local function before_warp()
+local function before_warp(lvl, area, node)
   
-  if curBGM ~= nil then
-      audio_stream_stop(curBGM)
-      curBGM = nil
+  if bgm.src ~= nil then
+      audio_stream_stop(bgm.src)
+      bgm.src = nil
+  end
+  
+  set_env_tint({r = 255, g = 255, b = 255}, {x = 0,y = 0, z = 0})
+  
+  if not MAPi.get_cur_hangout() then return end
+  
+  local curMap = mapTable[curLevel]
+  
+  if lvl == 64 then
+    return {
+      destLevel = curMap.source,
+      destArea = area,
+      destNode = node
+    }
+  end
+  
+  if curMap.codeWarps and curMap.codeWarps[lvl] then
+    if MAPi.get_hangout_from_levelnum(curMap.codeWarps[lvl]) then
+      curLevel = MAPi.get_hangout_from_levelnum(curMap.codeWarps[lvl])
+    end
+    warp_to_warpnode(curMap.codeWarps[lvl], area, gNetworkPlayers[0].currActNum, node)
   end
   
 end
@@ -146,14 +170,16 @@ end
 hook_event(HOOK_BEFORE_WARP, before_warp)
 
 
-local function on_warp()
+function on_warp()
+  curMap = mapTable[curLevel]
+  curArea = gNetworkPlayers[0].currAreaIndex
   
   local l = gLakituState
     local skyboxcheck = obj_get_nearest_object_with_behavior_id(o, id_bhvmapiskybox)
     local p = gNetworkPlayers[0]
     if skyboxcheck == nil and mapTable[curLevel].skybox ~= {nil} and (MAPi.get_levelnum_from_hangout(curLevel) ~= nil and gNetworkPlayers[0].currLevelNum == MAPi.get_levelnum_from_hangout(curLevel)) then
       local sky = mapTable[curLevel].skybox
-      if sky[p.currAreaIndex] ~= nil then
+      if sky[p.currAreaIndex] then
       if sky[p.currAreaIndex].skytype == "ico" then
         spawn_non_sync_object(id_bhvmapiskybox, E_MODEL_SKYBOX_CUSTOM_ICO, l.pos.x, l.pos.y, l.pos.z, nil)
       elseif sky[p.currAreaIndex].skytype == "box" then
@@ -163,28 +189,27 @@ local function on_warp()
       end
     end
   
-  if curBGM ~= nil then
-    audio_stream_stop(curBGM)
+  if bgm.src then
+    audio_stream_stop(bgm.src)
     end
   
-if gNetworkPlayers[0].currLevelNum == MAPi.get_levelnum_from_hangout(curLevel) and mapTable[curLevel].bgm ~= nil then
+if gNetworkPlayers[0].currLevelNum == MAPi.get_levelnum_from_hangout(curLevel) and mapTable[curLevel].bgm then
   if type(mapTable[curLevel].bgm) == "table" then
-    for i, bgm in pairs(mapTable[curLevel].bgm) do
+    for i, musc in pairs(mapTable[curLevel].bgm) do
     if i == gNetworkPlayers[0].currAreaIndex then
-    stop_background_music(get_current_background_music())
-    curBGM = bgm.isStream == true and bgm or nil
+    bgm.src = musc.isStream == true and musc or nil
     end
     end
     else
-      stop_background_music(get_current_background_music())
-      if curBGM ~= nil then
-    audio_stream_stop(curBGM)
+      if bgm.src then
+    audio_stream_stop(bgm.src)
     end
-    curBGM = mapTable[curLevel].bgm.isStream == true and mapTable[curLevel].bgm or nil
-    end
+    bgm.src = mapTable[curLevel].bgm.isStream == true and mapTable[curLevel].bgm or nil
+  end
+  stop_background_music(get_current_background_music())
 end
 
-if gNetworkPlayers[0].currLevelNum == MAPi.get_levelnum_from_hangout(curLevel) and mapTable[curLevel].envtint[p.currAreaIndex] ~= nil then
+if gNetworkPlayers[0].currLevelNum == MAPi.get_levelnum_from_hangout(curLevel) and mapTable[curLevel].envtint[p.currAreaIndex] then
   local env = mapTable[curLevel].envtint[p.currAreaIndex]
  set_env_tint(env.color, env.dir)
  else
@@ -195,40 +220,47 @@ end
 
 hook_event(HOOK_ON_WARP, on_warp)
 
-function play_custom_bgm(m)
+
+function play_custom_bgm()
+  m = gMarioStates[0]
+  curMap = mapTable[MAPi.get_cur_hangout()]
   if m.playerIndex ~= 0 then
     return end
   
-  if curBGM ~= nil then
-  audio_stream_play(curBGM, false, curVolume)
-  audio_stream_set_looping(curBGM, true)
+  if bgm.src then
+  audio_stream_play(bgm.src, false, bgm.volume)
+  audio_stream_set_looping(bgm.src, true)
   
-  if m.numLives < 0 then
-    audio_stream_stop(curBGM)
-    curBGM = nil
+  if gNetworkPlayers[0].currLevelNum ~= mapTable[curLevel].source then
+    audio_stream_stop(bgm.src)
+    bgm.src = nil
+    set_env_tint({r = 255, g = 255, b = 255}, {x = 0,y = 0, z = 0})
     end
   
   end
   
   if m.flags & MARIO_WING_CAP ~= 0 or m.flags & MARIO_VANISH_CAP ~= 0 or m.flags & MARIO_METAL_CAP ~= 0 or m.action & ACT_FLAG_RIDING_SHELL ~= 0 then
-   curVolume = VOLUME_MUTE
+   bgm.goalVolume = VOLUME_MUTE
     else 
       if is_game_paused() == true or MAPi.is_menu_open() == true then
-        curVolume = VOLUME_PAUSE
+        bgm.goalVolume = VOLUME_PAUSE
         else
-      curVolume = VOLUME_DEFAULT
+      bgm.goalVolume = VOLUME_DEFAULT
       end
   end
-  
-  if (_G.charSelectExists) then
-    if (_G.charSelect.is_menu_open()) then
-        curVolume = VOLUME_MUTE
+  if (charSelectExists) then
+    if (charSelect.is_menu_open()) then
+      if charSelect.get_option(charSelect.optionTableRef.music).toggle == 1 or charSelect.get_option(charSelect.optionTableRef.music).toggle == 2 then
+        bgm.goalVolume = VOLUME_MUTE
       end
     end
+  end
+  
+  bgm.volume = lerp(bgm.volume, bgm.goalVolume, 0.2)
   
 end
 
-hook_event(HOOK_MARIO_UPDATE, play_custom_bgm)
+hook_event(HOOK_UPDATE, play_custom_bgm)
 
 
 local function what_maps()
@@ -269,7 +301,6 @@ local function mapi_warp_command(msg)
     djui_popup_create("This map does not Exist!", 1)
     return true
     else
-      play_transition(WARP_TRANSITION_FADE_FROM_BOWSER, 30, 0, 0, 0)
       warp_to_hangout(map, 1, (map == 2 and 0x20 or map == 1 and 0xFF) or 0x0A)
       return true
   end
@@ -344,6 +375,15 @@ end
 hook_event(HOOK_ON_PACKET_RECEIVE, packet_receive)
 
 local function on_mods_load()
+  
+  for i, mod in pairs(gActiveMods) do
+    if mod.incompatible == "romhack" then
+      IS_ROMHACK = true
+    end
+  end
+  
+  mapTable[1].source = gLevelValues.entryLevel
+  
   local hangouts = #mapTable - 2
   local TEXT_BIND = SETTING_LTRIG == true and " use Start + L or \\#00ffff\\/mapi-warp\\#dcdcdc\\ to open the menu" or " use \\#00ffff\\/mapi-warp\\#dcdcdc\\ to open the menu"
   local string = "Mapi currently has \\#00ff00\\".. tostring(hangouts).. "\\#dcdcdc\\ maps available! ".. TEXT_BIND
